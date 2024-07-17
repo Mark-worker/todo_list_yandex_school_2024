4c834f4e2c630536927d7528629a554d3d1ad080
mark-120 <78369499+Mark-120@users.noreply.github.com>
2024-07-18 01:20:20 +0300

    Added light and dark themes in thw uikit

--- lib/uikit/styles.dart
+++ lib/uikit/styles.dart
@@ -1,30 +1,30 @@
 import 'package:flutter/material.dart';
 
-class Styles {
-  final TextStyle largeTitleStyle = const TextStyle(
+abstract class AppTextStyle {
+  static final TextStyle largeTitleStyle = const TextStyle(
     fontSize: 32,
     height: 38 / 32,
     fontWeight: FontWeight.bold,
     fontFamily: "Roboto",
   );
-  final TextStyle titleStyle = const TextStyle(
+  static final TextStyle titleStyle = const TextStyle(
     fontSize: 20,
     height: 32 / 20,
     fontWeight: FontWeight.bold,
     fontFamily: "Roboto",
   );
-  final buttonStyle = const TextStyle(
+  static final buttonStyle = const TextStyle(
     fontSize: 14,
     height: 24 / 14,
     fontWeight: FontWeight.bold,
     fontFamily: "Roboto",
   );
-  final TextStyle bodyStyle = const TextStyle(
+  static final TextStyle bodyStyle = const TextStyle(
     fontSize: 16,
     height: 20 / 16,
     fontFamily: "Roboto",
   );
-  final TextStyle subheadStyle = const TextStyle(
+  static final TextStyle subheadStyle = const TextStyle(
     fontSize: 14,
     height: 20 / 14,
     fontFamily: "Roboto",
