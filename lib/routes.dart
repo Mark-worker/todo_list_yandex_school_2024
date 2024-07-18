858197bbc4d7143d2f0d6c4a5019fa4e6da421b7
mark-120 <78369499+Mark-120@users.noreply.github.com>
2024-07-18 07:56:34 +0300

    Added some small changes for GitHub Actions

--- lib/routes.dart
+++ lib/routes.dart
@@ -4,8 +4,8 @@ import "package:todo_list_yandex_school_2024/feature/presentation/edit_task_scre
 import "package:todo_list_yandex_school_2024/feature/presentation/main_screen.dart";
 
 final routes = RouteMap(routes: {
-  "/": (_) => MaterialPage(child: MainPage()),
+  "/": (_) => const MaterialPage(child: MainPage()),
   "/editing/:id": (route) =>
       MaterialPage(child: EditTaskPage(editingTaskId: route.pathParameters["id"])),
-  "/creating": (_) => MaterialPage(child: EditTaskPage()),
-});
+  "/creating": (_) => const MaterialPage(child: EditTaskPage()),
+},);
